d9da14e26840b0647776166e00b07df29d4ff65e
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2019-06-26 12:53:39 -0400

    adding thu

--- TpuSummary.py
+++ TpuSummary.py
@@ -5,13 +5,16 @@ import rasterio
 import rasterio.merge
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from matplotlib.gridspec import GridSpec
 import os
 import seaborn as sns
 import numpy as np
+import pandas as pd
+
 
 sns.set(font_scale=1.5)
 sns.set_style("whitegrid")
-plt.rcParams['figure.figsize'] = (8, 8)
+plt.rcParams['figure.figsize'] = (12, 8)
 
 las_dir = Path('C:/QAQC_contract/marco_island/tpu_output')
 las_files = list(las_dir.glob('*.las'))
@@ -20,9 +23,10 @@ for las in las_files:
     las_str = str(las).replace('\\', '/')
     print(las.name)
 
-    gtiff_path = 'Z:/cBLUE/summary_test/{}.tif'.format(las.stem)
+    gtiff_path_thu = 'Z:/cBLUE/summary_test/{}_THU.tif'.format(las.stem)
+    gtiff_path_tvu = 'Z:/cBLUE/summary_test/{}_TVU.tif'.format(las.stem)
 
-    pdal_json = """{
+    pdal_jsons = """{
         "pipeline":[
             {
                 "type": "readers.las",
@@ -37,7 +41,7 @@ for las in las_files:
             {
                 "inputs": ["A"],
                 "dimension": "total_tvu",
-                "filename": """ + '"{}"'.format(gtiff_path) + """,
+                "filename": """ + '"{}"'.format(gtiff_path_tvu) + """,
                 "gdaldriver": "GTiff",
                 "output_type": "mean",
                 "resolution": "1.0",
@@ -46,7 +50,8 @@ for las in las_files:
         ]
     }"""
 
-    pipeline = pdal.Pipeline(pdal_json)
+    
+    pipeline = pdal.Pipeline(p)
     #pipeline.validate()
 
     count = pipeline.execute()
@@ -54,25 +59,61 @@ for las in las_files:
 
 
 tpu_dem_dir = Path('Z:/cBLUE/summary_test')
-tpu_dems = list(tpu_dem_dir.glob('*.tif'))
-tpu_dems_to_mosaic = []
 
-for tpu_dem in tpu_dems:
-    src = rasterio.open(tpu_dem)
-    tpu_dems_to_mosaic.append(src)    
+tpu_thu_dems = list(tpu_dem_dir.glob('*_THU.tif'))
+tpu_thu_dems_to_mosaic = []
+
+tpu_tvu_dems = list(tpu_dem_dir.glob('*_TVU.tif'))
+tpu_tvu_dems_to_mosaic = []
+
+for tpu_thu_dem in tpu_thu_dems:
+    src = rasterio.open(tpu_thu_dem)
+    tpu_thu_dems_to_mosaic.append(src)    
     
-print(tpu_dems_to_mosaic)
-mosaic, out_trans = rasterio.merge.merge(tpu_dems_to_mosaic)
-mosaic[mosaic < 0] = np.nan
-print(mosaic)
+for tpu_tvu_dem in tpu_tvu_dems:
+    src = rasterio.open(tpu_tvu_dem)
+    tpu_tvu_dems_to_mosaic.append(src)  
 
-fig, ax = plt.subplots(1, 2, figsize=(10,10))
-im = ax[0].imshow(mosaic.squeeze(), cmap='jet', vmin=20, vmax=50)
-cax = fig.add_axes([0.1, 0.05, 0.35, 0.02])
-fig.colorbar(im, cax=cax, orientation='horizontal')
 
-ax[1].hist(mosaic.ravel(), bins=100, color='gray')
-ax[1].set_xlim(0, 50)
-ax[1].set(xlabel='Total TVU (cm)', ylabel='Count', title='Total TVU')
+mosaic_thu, out_trans = rasterio.merge.merge(tpu_thu_dems_to_mosaic)
+mosaic_thu[mosaic_thu < 0] = np.nan
+
+mosaic_tvu, out_trans = rasterio.merge.merge(tpu_tvu_dems_to_mosaic)
+mosaic_tvu[mosaic_tvu < 0] = np.nan
+
+
+min_x = 0
+max_x = 50
+
+fig = plt.figure(constrained_layout=True)
+fig.suptitle('cBLUE TPU Results Summary')
+
+gs = GridSpec(3, 2, width_ratios=[1, 1], height_ratios=[1, 1, 1], hspace=0.5, wspace=0.35)
+ax0 = fig.add_subplot(gs[:, 0])
+ax1 = fig.add_subplot(gs[0, 1])
+ax2 = fig.add_subplot(gs[1, 1])
+ax3 = fig.add_subplot(gs[2, 1])
+
+im = ax0.imshow(mosaic_tvu.squeeze(), cmap='jet', vmin=20, vmax=50)
+cax = fig.add_axes([0.05, 0.3, 0.02, 0.3])
+fig.colorbar(im, cax=cax, orientation='vertical')
+
+tpu_stats = np.asarray([
+    [np.nanmin(mosaic_thu), np.nanmax(mosaic_thu), np.nanmean(mosaic_thu), np.nanstd(mosaic_thu)],
+    [np.nanmin(mosaic_tvu), np.nanmax(mosaic_tvu), np.nanmean(mosaic_tvu), np.nanstd(mosaic_tvu)]]).T
+df_idx = ['min', 'max', 'mean', 'std']
+df_cols = ['Total THU', 'Total TVU']
+ax1.axis('tight')
+ax1.axis('off')
+ax1.table(cellText=tpu_stats.round(1), colLabels=df_cols, rowLabels=df_idx, bbox=[0, 0, 1, 1])
+
+ax2.hist([-1], bins=100, color='gray')
+ax2.set_xlim(min_x, max_x)
+ax2.set_xticklabels([])
+ax2.set(xlabel=None, ylabel='Count', title='Total THU')
+
+ax3.hist(mosaic_tvu.ravel(), bins=100, color='gray')
+ax3.set_xlim(min_x, max_x)
+ax3.set(xlabel='(cm)', ylabel='Count', title='Total TVU')
 
 plt.show()
