8c0ae78ef282f30e83bc3bdc83833eecd116be11
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2020-03-12 16:53:18 -0400

    deleted certain print statements and changed version number before release v2.2.0

--- Tpu.py
+++ Tpu.py
@@ -50,8 +50,6 @@ from Las import Las
 import datetime
 import cProfile
 
-import matplotlib.pyplot as plt
-
 
 class Tpu:
     """
@@ -121,190 +119,10 @@ class Tpu:
                              fl_tpu_stddev[ind])
         
             fl_stats_str = '{}: {:6.3f}{:6.3f}{:6.3f}{:6.3f}'.format(*fl_stats_vals)
-            #fl_stats_str = '{}: {:6d}{:6d}{:6.0f}{:6.0f}'.format(*fl_stats_vals)
             fl_stats_strs.append(fl_stats_str)
 
         fl_header_str = f'{fl} ({fl_tpu_count}/{num_fl_points} points with TPU)'
         self.flight_line_stats.update({fl_header_str: fl_stats_strs})
-
-    def export_diag_data(self, las_base_name, diag_dfs):
-        diag_data_fields = {
-            'las_data': [
-                'las_name', 
-                'flight_line', 
-                'raw_class',
-                'las_t', 
-                'las_x', 
-                'las_y', 
-                'las_z'],
-            'sbet_data': [
-                'sbet_t', 
-                'sbet_x', 
-                'sbet_y', 
-                'sbet_z', 
-                'r', 
-                'p', 
-                'h', 
-                'std_r', 
-                'std_p', 
-                'std_h', 
-                'stdx_sbet', 
-                'stdy_sbet', 
-                'stdz_sbet'],
-            'subaerial_data': [
-                'rho_est', 
-                'a_est', 
-                'b_est', 
-                'std_a', 
-                'std_b', 
-                'std_rho', 
-                'aer_x_pre_poly', 
-                'aer_y_pre_poly', 
-                'aer_z_pre_poly', 
-                'dx', 
-                'dy', 
-                'dz'],
-            'cblue_position': [
-                'cblue_aer_x', 
-                'cblue_aer_y', 
-                'cblue_aer_z', 
-                'cblue_aer_err_x', 
-                'cblue_aer_err_y', 
-                'cblue_aer_err_z'],
-            'subaer_comp_uncertainties': [
-                'x_subaer_a', 
-                'x_subaer_b', 
-                'x_subaer_r', 
-                'x_subaer_p', 
-                'x_subaer_h', 
-                'x_subaer_x', 
-                'x_subaer_y', 
-                'x_subaer_z', 
-                'x_subaer_rho', 
-                'y_subaer_a', 
-                'y_subaer_b', 
-                'y_subaer_r', 
-                'y_subaer_p', 
-                'y_subaer_h', 
-                'y_subaer_x', 
-                'y_subaer_y', 
-                'y_subaer_z', 
-                'y_subaer_rho', 
-                'z_subaer_a', 
-                'z_subaer_b', 
-                'z_subaer_r', 
-                'z_subaer_p', 
-                'z_subaer_h', 
-                'z_subaer_x', 
-                'z_subaer_y', 
-                'z_subaer_z', 
-                'z_subaer_rho'],
-            'tpu_data': [
-                'subaerial_thu', 
-                'subaerial_tvu', 
-                'subaqueous_thu', 
-                'subaqueous_tvu', 
-                'total_thu', 
-                'total_tvu']
-            }
-
-        for k, v in diag_data_fields.items():
-            data = {}
-            for field in v:
-                print(f'adding {field} data to array...')
-                data[field] = []
-                for fl_df in diag_dfs:
-                    data[field].extend(fl_df[field].to_list())
-            
-            df = pd.DataFrame(data)
-            out_path = Path(self.tpu_output) / f'{las_base_name}_{k}.pkl'
-            print(f'pickling {out_path}...')
-            df.to_pickle(str(out_path))
-
-    def output_diagnostic_data(self, las_short_name, fl, merged_data, 
-                               stddev, subaer_obj, subaqu_obj, 
-                               total_thu, total_tvu, raw_class):
-
-        print(f'outputting diagnostic data - {las_short_name}, flight line {fl}...')
-
-        las_pos_xyz = (merged_data[2], merged_data[3], merged_data[4], )
-        sensor_model_diag_data = subaer_obj.jacobian.sensor_model.get_sensor_model_diagnostic_data(las_pos_xyz)
-
-        diag_data = {
-            'las_name': las_short_name,
-            'flight_line': fl,
-            'raw_class': raw_class,
-            'las_t': merged_data[1],
-            'las_x': merged_data[2],
-            'las_y': merged_data[3],
-            'las_z': merged_data[4],
-            'sbet_t': merged_data[0],
-            'sbet_x': merged_data[5],
-            'sbet_y': merged_data[6],
-            'sbet_z': merged_data[7],
-            'r': merged_data[8],
-            'p': merged_data[9],
-            'h': merged_data[10],
-            'std_a': stddev[0],
-            'std_b': stddev[1],
-            'std_r': stddev[2],
-            'std_p': stddev[3],
-            'std_h': stddev[4],
-            'stdx_sbet': stddev[5],
-            'stdy_sbet': stddev[6],
-            'stdz_sbet': stddev[7],
-            'std_rho': stddev[8],
-            'rho_est': sensor_model_diag_data[0],
-            'a_est': sensor_model_diag_data[1],
-            'b_est': sensor_model_diag_data[2],
-            'aer_x_pre_poly': sensor_model_diag_data[3],
-            'aer_y_pre_poly': sensor_model_diag_data[4],
-            'aer_z_pre_poly': sensor_model_diag_data[5],
-            'dx': sensor_model_diag_data[6],
-            'dy': sensor_model_diag_data[7],
-            'dz': sensor_model_diag_data[8],
-            'cblue_aer_x': sensor_model_diag_data[9],  # np.round_(total_thu * 100).astype('int')
-            'cblue_aer_y': sensor_model_diag_data[10],
-            'cblue_aer_z': sensor_model_diag_data[11],
-            'cblue_aer_err_x': sensor_model_diag_data[12],
-            'cblue_aer_err_y': sensor_model_diag_data[13],
-            'cblue_aer_err_z': sensor_model_diag_data[14],
-            'x_subaer_a': np.round_(subaer_obj.x_comp_uncertainties[0] * 1000).astype('uint16'),
-            'x_subaer_b': np.round_(subaer_obj.x_comp_uncertainties[1] * 1000).astype('uint16'),
-            'x_subaer_r': np.round_(subaer_obj.x_comp_uncertainties[2] * 1000).astype('uint16'),
-            'x_subaer_p': np.round_(subaer_obj.x_comp_uncertainties[3] * 1000).astype('uint16'),
-            'x_subaer_h': np.round_(subaer_obj.x_comp_uncertainties[4] * 1000).astype('uint16'),
-            'x_subaer_x': np.round_(subaer_obj.x_comp_uncertainties[5] * 1000).astype('uint16'),
-            'x_subaer_y': None,
-            'x_subaer_z': None,
-            'x_subaer_rho': np.round_(subaer_obj.x_comp_uncertainties[6] * 1000).astype('uint16'),
-            'y_subaer_a': np.round_(subaer_obj.y_comp_uncertainties[0] * 1000).astype('uint16'),
-            'y_subaer_b': np.round_(subaer_obj.y_comp_uncertainties[1] * 1000).astype('uint16'),
-            'y_subaer_r': np.round_(subaer_obj.y_comp_uncertainties[2] * 1000).astype('uint16'),
-            'y_subaer_p': np.round_(subaer_obj.y_comp_uncertainties[3] * 1000).astype('uint16'),
-            'y_subaer_h': np.round_(subaer_obj.y_comp_uncertainties[4] * 1000).astype('uint16'),
-            'y_subaer_x': None,
-            'y_subaer_y': np.round_(subaer_obj.y_comp_uncertainties[5] * 1000).astype('uint16'),
-            'y_subaer_z': None,
-            'y_subaer_rho': np.round_(subaer_obj.y_comp_uncertainties[6] * 1000).astype('uint16'),
-            'z_subaer_a': np.round_(subaer_obj.z_comp_uncertainties[0] * 1000).astype('uint16'),
-            'z_subaer_b': np.round_(subaer_obj.z_comp_uncertainties[1] * 1000).astype('uint16'),
-            'z_subaer_r': np.round_(subaer_obj.z_comp_uncertainties[2] * 1000).astype('uint16'),
-            'z_subaer_p': np.round_(subaer_obj.z_comp_uncertainties[3] * 1000).astype('uint16'),
-            'z_subaer_h': None,
-            'z_subaer_x': None,
-            'z_subaer_y': None,
-            'z_subaer_z': np.round_(subaer_obj.z_comp_uncertainties[4] * 1000).astype('uint16'),
-            'z_subaer_rho': np.round_(subaer_obj.z_comp_uncertainties[5] * 1000).astype('uint16'),
-            'subaerial_thu': subaer_obj.thu,
-            'subaerial_tvu': subaer_obj.tvu,
-            'subaqueous_thu': subaqu_obj.thu,
-            'subaqueous_tvu': subaqu_obj.tvu,
-            'total_thu': total_thu,
-            'total_tvu': total_tvu,
-            }
-
-        return pd.DataFrame(diag_data)
 
     def calc_tpu(self, sbet_las_files):
         """
@@ -367,27 +185,14 @@ class Tpu:
                     total_tvu = ne.evaluate('sqrt(subaer_tvu**2 + subaqu_tvu**2 + vdatum_mcu**2)')
 
                     fl_tpu_data = np.vstack((
-                        #np.round_(total_thu * 1000).astype('uint16'),
-                        #np.round_(total_tvu * 1000).astype('uint16'),
                         total_thu,
                         total_tvu,
-                        merged_data[1],
-                        merged_data[2],
-                        merged_data[3],
-                        merged_data[4],
-                        raw_class,
                         unsort_idx
                         )).T
 
-                    print(fl_tpu_data)
                     data_to_output.append(fl_tpu_data)
 
                     self.update_fl_stats(fl, num_fl_points, fl_tpu_data)
-
-                    #diag_dfs.append(self.output_diagnostic_data(las.las_short_name, fl,
-                    #                                            merged_data, stddev, 
-                    #                                            subaer_obj, subaqu_obj, 
-                    #                                            total_thu, total_tvu, raw_class))
 
                 else:
                     logging.warning('SBET and LAS not merged because max delta '
@@ -399,9 +204,6 @@ class Tpu:
 
             self.write_metadata(las)  # TODO: include as VLR?
             self.output_tpu_to_las_extra_bytes(las, data_to_output)
-
-            ## merge fl diagnostic dataframes
-            #self.export_diag_data(las.las_base_name, diag_dfs)
 
         else:
             logging.warning('WARNING: {} has no data points'.format(las.las_short_name))
@@ -446,17 +248,11 @@ class Tpu:
         in_las = laspy.file.File(las.las, mode="r")  # las is Las object
         out_las = laspy.file.File(out_las_name, mode="w", header=in_las.header)
 
-        tpu_data_type = 9  # 3 = laspy unsigned short (2 bytes)
+        tpu_data_type = 9  # float
 
         extra_byte_dimensions = OrderedDict([
             ('total_thu', tpu_data_type),
-            ('total_tvu', tpu_data_type),
-            ('t', tpu_data_type),
-            ('x', tpu_data_type),
-            ('y', tpu_data_type),
-            ('z', tpu_data_type),
-            ('raw_class', tpu_data_type),
-            ('unsort_idx', tpu_data_type)
+            ('total_tvu', tpu_data_type)
             ])
 
         num_extra_bytes = len(extra_byte_dimensions.keys())
@@ -470,12 +266,10 @@ class Tpu:
 
         if len(data_to_output) != 0:
             tpu_data = np.vstack(data_to_output)
-            print(tpu_data)
-            extra_byte_df = pd.DataFrame(tpu_data, 
-                                         index=tpu_data[:, -1], 
+            extra_byte_df = pd.DataFrame(tpu_data[:, 0:num_extra_bytes], 
+                                         index=tpu_data[:, num_extra_bytes], 
                                          columns=extra_byte_dimensions.keys())
-            print(extra_byte_df)
-                
+      
             if extra_byte_df.shape[0] == las.num_file_points:
                 extra_byte_df = extra_byte_df.sort_index()
             else:
@@ -490,27 +284,7 @@ class Tpu:
         
             logging.debug('populating extra byte data for total_tvu...')
             out_las.total_tvu = extra_byte_df['total_tvu']
-            
-            logging.debug('populating extra byte data for t...')
-            out_las.t = extra_byte_df['t']
         
-            logging.debug('populating extra byte data for x...')
-            out_las.x = extra_byte_df['x']
-
-            logging.debug('populating extra byte data for y...')
-            out_las.y = extra_byte_df['y']
-        
-            logging.debug('populating extra byte data for z...')
-            out_las.z = extra_byte_df['z']
-
-            logging.debug('populating extra byte data for raw_class...')
-            out_las.raw_class = extra_byte_df['raw_class']
-
-            logging.debug('populating extra byte data for unsort_idx...')
-            out_las.unsort_idx = extra_byte_df['unsort_idx']
-
-            #logging.debug('populating extra byte data for masked_fl_t_idx...')
-            #out_las.masked_fl_t_idx = extra_byte_df['masked_fl_t_idx']
         else:
             logging.debug('populating extra byte data for total_thu...')
             out_las.total_thu = np.zeros(las.num_file_points)
@@ -575,7 +349,7 @@ class Tpu:
         """
 
         print('Calculating TPU (multi-processing)...')
-        p = pp.ProcessPool(4)
+        p = pp.ProcessPool(2)
 
         for _ in tqdm(p.imap(self.calc_tpu, sbet_las_generator), total=num_las, ascii=True):
             pass
