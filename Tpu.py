b92758d40b0afeda915f4a9f85d6778a4b8f0e09
kiefk <kiefk@oregonstate.edu>
2023-05-16 18:20:55 -0700

    Use xyz_to_decimal() to convert xyz int values
    The converted values will print to the csv file
    
    Instead of the raw X, Y, and Z integer point values

--- Tpu.py
+++ Tpu.py
@@ -30,7 +30,7 @@ christopher.parrish@oregonstate.edu
 
 Last Edited By:
 Keana Kief (OSU)
-April 25th, 2023
+May 16th, 2023
 
 """
 
@@ -373,13 +373,18 @@ class Tpu:
             # get name of csv from las file
             out_csv_name = os.path.join(self.gui_object.output_directory, las.las_base_name) + "_TPU.csv"
 
+            csv_las = Las(out_las_name)
+
+            #xyz_to_decimal converts the x, y, z integer values to decimal values
+            x, y, z = csv_las.xyz_to_decimal()
+
             # Save relevant data to csv
             pd.DataFrame.from_dict(
                 {
                     "GPS Time": out_las.gps_time,
-                    "X": out_las.X,
-                    "Y": out_las.Y,
-                    "Z": out_las.Z,
+                    "X": x,
+                    "Y": y,
+                    "Z": z,
                     "THU": out_las.total_thu,
                     "TVU": out_las.total_tvu,
                     "Classification": out_las.classification,
